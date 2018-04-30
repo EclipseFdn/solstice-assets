8282979cae159c16d7fee957b1c29cc013b8984a
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2018-04-30 13:56:46 -0400

    add epl-v2 headers
    Change-Id: I5cab1375cd4b7316ebfac3f6119dc57c83514a21
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,28 @@
 let mix = require('laravel-mix');
+mix.options({uglify: {uglifyOptions: {compress: false, output: {comments: true}}}});
 mix.setPublicPath('dist');
-mix.less('less/quicksilver/styles.less', 'dist/stylesheets/quicksilver.min.css');
-mix.less('less/quicksilver/jakarta/styles.less', 'dist/stylesheets/jakarta.min.css');
-mix.less('less/quicksilver/eclipse-ide/styles.less', 'dist/stylesheets/eclipse-ide.min.css');
-mix.less('less/solstice/_barebone/styles.less', 'dist/stylesheets/barebone.min.css');
-mix.less('less/solstice/_barebone/footer.less', 'dist/stylesheets/barebone-footer.min.css');
-mix.less('less/solstice/table.less', 'dist/stylesheets/table.min.css');
-mix.less('less/solstice/locationtech/styles.less', 'dist/stylesheets/locationtech.min.css');
-mix.less('less/solstice/locationtech/barebone.less', 'dist/stylesheets/locationtech-barebone.min.css');
-mix.less('less/solstice/polarsys/styles.less', 'dist/stylesheets/polarsys.min.css');
-mix.less('less/solstice/polarsys/barebone.less', 'dist/stylesheets/polarsys-barebone.min.css');
-mix.less('less/solstice/styles.less', 'dist/stylesheets/styles.min.css');
+mix.setResourceRoot('../');
+
+// Default CSS
+mix.less('less/quicksilver/styles.less', 'dist/css/quicksilver.css');
+mix.less('less/quicksilver/jakarta/styles.less', 'dist/css/jakarta.css');
+mix.less('less/quicksilver/eclipse-ide/styles.less', 'dist/css/eclipse-ide.css');
+mix.less('less/solstice/_barebone/styles.less', 'dist/css/barebone.css');
+mix.less('less/solstice/_barebone/footer.less', 'dist/css/barebone-footer.css');
+mix.less('less/solstice/table.less', 'dist/css/table.css');
+mix.less('less/solstice/locationtech/styles.less', 'dist/css/locationtech.css');
+mix.less('less/solstice/locationtech/barebone.less', 'dist/css/locationtech-barebone.css');
+mix.less('less/solstice/polarsys/styles.less', 'dist/css/polarsys.css');
+mix.less('less/solstice/polarsys/barebone.less', 'dist/css/polarsys-barebone.css');
+mix.less('less/solstice/styles.less', 'dist/css/solstice.css');
+
+// Drupal sites
+mix.less('less/solstice/drupal.less', 'dist/css/drupal-solstice.css');
+
+// JavaScript
+mix.scripts([
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    './node_modules/feather-icons/dist/feather.min.js',
+    './js/solstice.js'
+], 'dist/js/solstice.js');
