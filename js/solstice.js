cfff5bc3e341739356bbd519c03664b3ba109fd8
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2018-07-31 11:40:19 -0400

    update list of extensions
    Change-Id: Ie396eb9e8d4dedc7e1a8a6d0e4a86c9219c6487c
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- js/solstice.js
+++ js/solstice.js
@@ -146,7 +146,7 @@
       // Quit here if the extension of the clicked file isn't part of the following list
       // and if the Google Analytics is not loaded
       var tracker = ga.getAll()[0].get('name');
-      if (tracker && $.inArray(fileExtension, ['pdf', 'jpg', 'png', 'zip', 'dmg', 'gz']) !== -1) {
+      if (tracker && $.inArray(fileExtension, ['pdf', 'jpg', 'png', 'zip', 'dmg', 'gz', 'exe', 'doc', 'odt', 'rtf', '7z', 'arj', 'deb', 'pkg', 'rar', 'rpm', 'z', 'tar', 'xml', 'csv', 'xls', 'xlr', 'ods', 'rss']) !== -1) {
         // Send the event to Google Analytics
         ga(tracker + '.send', 'event', {
           'eventCategory': 'solstice-event-tracker',
