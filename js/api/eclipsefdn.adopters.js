802530ad063b4990f64c18653e3d3f4ce207338f
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2021-07-16 15:31:07 -0400

    update to better support drupal (#266)
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- js/api/eclipsefdn.adopters.js
+++ js/api/eclipsefdn.adopters.js
@@ -10,231 +10,233 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
+const EclipseFdnAdopters = (function (window, document) {
+  class EclipseFdnAdoptersClass {
+    precompiledRegex =
+      /<([^>]*?)>;(\s?[\w-]*?="(?:\\"|[^"])*";){0,}\s?rel="next"/;
 
-class EclipseFdnAdopters {
-  precompiledRegex = /<([^>]*?)>;(\s?[\w-]*?="(?:\\"|[^"])*";){0,}\s?rel="next"/;
+    // Default settings
+    default_options = {
+      project_id: '',
+      selector: '.eclipsefdn-adopters',
+      ul_classes: '',
+      logo_white: false,
+      working_group: '',
+      src_root: 'https://api.eclipse.org/adopters',
+      src_projects_prefix: '/projects',
+    };
 
-  // Default settings
-  default_options = {
-    project_id: '',
-    selector: '.eclipsefdn-adopters',
-    ul_classes: '',
-    logo_white: false,
-    working_group: '',
-    src_root: 'https://api.eclipse.org/adopters',
-    src_projects_prefix: '/projects',
-  };
+    getMergedOptions(options) {
+      // Default settings copy
+      var opts = JSON.parse(JSON.stringify(this.default_options));
 
-  getMergedOptions(options) {
-    // Default settings copy
-    var opts = JSON.parse(JSON.stringify(this.default_options));
-
-    // Go through the parameters of Options if its defined and is an object
-    if (typeof options !== 'undefined' && typeof options === 'object') {
-      for (var optionName in this.default_options) {
-        if (
-          typeof options[optionName] === 'undefined' ||
-          (typeof options[optionName] !== 'string' &&
-            typeof options[optionName] !== 'boolean')
-        ) {
-          continue;
+      // Go through the parameters of Options if its defined and is an object
+      if (typeof options !== 'undefined' && typeof options === 'object') {
+        for (var optionName in this.default_options) {
+          if (
+            typeof options[optionName] === 'undefined' ||
+            (typeof options[optionName] !== 'string' &&
+              typeof options[optionName] !== 'boolean')
+          ) {
+            continue;
+          }
+          opts[optionName] = options[optionName];
         }
-        opts[optionName] = options[optionName];
       }
+      return opts;
     }
-    return opts;
-  }
 
-  /**
-   * Replace the adopters container
-   * @public
-   * @param {Object} options Videos attributes
-   */
+    /**
+     * Replace the adopters container
+     * @public
+     * @param {Object} options Videos attributes
+     */
 
-  getList = function (options) {
-    var t = this;
-    var opts = this.getMergedOptions(options);
-    this.fireCall(opts, function (response) {
-      t.createProjectList(
-        response,
-        opts,
-        document.querySelectorAll(opts.selector)
-      );
-      t.scrollToAnchor();
-    });
-  };
+    getList = function (options) {
+      var t = this;
+      var opts = this.getMergedOptions(options);
+      this.fireCall(opts, function (response) {
+        t.createProjectList(
+          response,
+          opts,
+          document.querySelectorAll(opts.selector)
+        );
+        t.scrollToAnchor();
+      });
+    };
 
-  /**
-   * Replace the adopters container
-   * @public
-   * @param {Object} options Videos attributes
-   */
+    /**
+     * Replace the adopters container
+     * @public
+     * @param {Object} options Videos attributes
+     */
 
-  getWGList = function (options) {
-    var t = this;
-    var opts = this.getMergedOptions(options);
-    // create callback on ready
-    this.fireCall(opts, function (response) {
-      t.createWGProjectsList(
-        response,
-        opts,
-        document.querySelectorAll(opts.selector)
-      );
-      t.scrollToAnchor();
-    });
-  };
+    getWGList = function (options) {
+      var t = this;
+      var opts = this.getMergedOptions(options);
+      // create callback on ready
+      this.fireCall(opts, function (response) {
+        t.createWGProjectsList(
+          response,
+          opts,
+          document.querySelectorAll(opts.selector)
+        );
+        t.scrollToAnchor();
+      });
+    };
 
-  fireCall(opts, callback, currentData = []) {
-    var t = this;
-    var xhttp = new XMLHttpRequest();
-    // create callback on ready
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        // merge new data with current
-        var json = JSON.parse(this.responseText);
-        if (Array.isArray(currentData) || currentData.length) {
-          json = currentData.concat(json);
-        }
+    fireCall(opts, callback, currentData = []) {
+      var t = this;
+      var xhttp = new XMLHttpRequest();
+      // create callback on ready
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          // merge new data with current
+          var json = JSON.parse(this.responseText);
+          if (Array.isArray(currentData) || currentData.length) {
+            json = currentData.concat(json);
+          }
 
-        // check the link header as long as its set
-        var linkHeader = xhttp.getResponseHeader('Link');
-        if (linkHeader !== null) {
-          var match = linkHeader.match(t.precompiledRegex);
-          // if there is no match, then there is no next and we are on the last page and should process data through callback
-          if (match !== null) {
-            opts.next = match[1];
-            t.fireCall(opts, callback, json);
+          // check the link header as long as its set
+          var linkHeader = xhttp.getResponseHeader('Link');
+          if (linkHeader !== null) {
+            var match = linkHeader.match(t.precompiledRegex);
+            // if there is no match, then there is no next and we are on the last page and should process data through callback
+            if (match !== null) {
+              opts.next = match[1];
+              t.fireCall(opts, callback, json);
+            } else {
+              callback(json);
+            }
           } else {
             callback(json);
           }
-        } else {
-          callback(json);
-        }
-      } else if (this.readyState == 4) {
-        console.log(
-          'Error while retrieving adopters data, could not complete operation'
-        );
-      }
-    };
-
-    // get the URL to call, using the 'next' url if set, otherwise building from original option set
-    var url;
-    if (opts.next !== undefined) {
-      url = opts.next;
-    } else {
-      url = opts.src_root + opts.src_projects_prefix;
-      if (opts.project_id !== undefined && opts.project_id.trim() !== '') {
-        url += '/' + opts.project_id;
-      }
-      if (
-        opts.working_group !== undefined &&
-        opts.working_group.trim() !== ''
-      ) {
-        url += '?working_group=' + opts.working_group;
-      }
-    }
-    // send request to get JSON data
-    xhttp.open('GET', url, true);
-    xhttp.send();
-  }
-
-  createWGProjectsList(json_object, opts, el) {
-    for (const project of json_object) {
-      var projectOpts = JSON.parse(JSON.stringify(opts));
-      projectOpts.project_id = project.project_id;
-
-      // add the title
-      const h2 = document.createElement('h2');
-      h2.textContent = project.name;
-      h2.setAttribute('id', project.project_id);
-      for (var i = 0; i < el.length; i++) {
-        el[i].appendChild(h2);
-      }
-      const headerAnchor = document.createElement('a');
-      headerAnchor.setAttribute(
-        'class',
-        'btn btn-xs btn-secondary margin-left-10 uppercase'
-      );
-      headerAnchor.setAttribute(
-        'href',
-        'https://projects.eclipse.org/projects/' + project.project_id
-      );
-      headerAnchor.textContent = project.project_id;
-      h2.appendChild(headerAnchor);
-
-      this.createProjectList(json_object, projectOpts, el);
-    }
-  }
-
-  createProjectList(json_object, opts, el) {
-    const ul = document.createElement('ul');
-    if (typeof json_object !== 'undefined') {
-      for (const project of json_object) {
-        if (opts.project_id !== project.project_id) {
-          continue;
-        }
-        for (const adopter of project.adopters) {
-          // Get the home page url of this adopter
-          var url = '';
-          if (typeof adopter['homepage_url'] !== 'undefined') {
-            url = adopter['homepage_url'];
-          }
-
-          // Get the name of this adopter
-          var name = '';
-          if (typeof adopter['name'] !== 'undefined') {
-            name = adopter['name'];
-          }
-
-          // Get the logo of this adopter
-          var logo = '';
-          if (typeof adopter['logo'] !== 'undefined') {
-            logo = adopter['logo'];
-          }
-          if (
-            opts['logo_white'] === true &&
-            typeof adopter['logo_white'] !== 'undefined'
-          ) {
-            logo = adopter['logo_white'];
-          }
-
-          // Create the html elements
-          let li = document.createElement('li');
-          let a = document.createElement('a');
-          let img = document.createElement('img');
-
-          a.setAttribute('href', url);
-          img.setAttribute('alt', name);
-          img.setAttribute(
-            'src',
-            opts.src_root + '/assets/images/adopters/' + logo
+        } else if (this.readyState == 4) {
+          console.log(
+            'Error while retrieving adopters data, could not complete operation'
           );
-          img.setAttribute('class', 'adopters-img');
+        }
+      };
 
-          a.appendChild(img);
-          li.appendChild(a);
-          ul.appendChild(li);
+      // get the URL to call, using the 'next' url if set, otherwise building from original option set
+      var url;
+      if (opts.next !== undefined) {
+        url = opts.next;
+      } else {
+        url = opts.src_root + opts.src_projects_prefix;
+        if (opts.project_id !== undefined && opts.project_id.trim() !== '') {
+          url += '/' + opts.project_id;
+        }
+        if (
+          opts.working_group !== undefined &&
+          opts.working_group.trim() !== ''
+        ) {
+          url += '?working_group=' + opts.working_group;
         }
       }
+      // send request to get JSON data
+      xhttp.open('GET', url, true);
+      xhttp.send();
     }
-    for (var i = 0; i < el.length; i++) {
-      if (opts['ul_classes'] !== '') {
-        ul.setAttribute('class', opts['ul_classes']);
+
+    createWGProjectsList(json_object, opts, el) {
+      for (const project of json_object) {
+        var projectOpts = JSON.parse(JSON.stringify(opts));
+        projectOpts.project_id = project.project_id;
+
+        // add the title
+        const h2 = document.createElement('h2');
+        h2.textContent = project.name;
+        h2.setAttribute('id', project.project_id);
+        for (var i = 0; i < el.length; i++) {
+          el[i].appendChild(h2);
+        }
+        const headerAnchor = document.createElement('a');
+        headerAnchor.setAttribute(
+          'class',
+          'btn btn-xs btn-secondary margin-left-10 uppercase'
+        );
+        headerAnchor.setAttribute(
+          'href',
+          'https://projects.eclipse.org/projects/' + project.project_id
+        );
+        headerAnchor.textContent = project.project_id;
+        h2.appendChild(headerAnchor);
+
+        this.createProjectList(json_object, projectOpts, el);
       }
-      el[i].appendChild(ul);
+    }
+
+    createProjectList(json_object, opts, el) {
+      const ul = document.createElement('ul');
+      if (typeof json_object !== 'undefined') {
+        for (const project of json_object) {
+          if (opts.project_id !== project.project_id) {
+            continue;
+          }
+          for (const adopter of project.adopters) {
+            // Get the home page url of this adopter
+            var url = '';
+            if (typeof adopter['homepage_url'] !== 'undefined') {
+              url = adopter['homepage_url'];
+            }
+
+            // Get the name of this adopter
+            var name = '';
+            if (typeof adopter['name'] !== 'undefined') {
+              name = adopter['name'];
+            }
+
+            // Get the logo of this adopter
+            var logo = '';
+            if (typeof adopter['logo'] !== 'undefined') {
+              logo = adopter['logo'];
+            }
+            if (
+              opts['logo_white'] === true &&
+              typeof adopter['logo_white'] !== 'undefined'
+            ) {
+              logo = adopter['logo_white'];
+            }
+
+            // Create the html elements
+            let li = document.createElement('li');
+            let a = document.createElement('a');
+            let img = document.createElement('img');
+
+            a.setAttribute('href', url);
+            img.setAttribute('alt', name);
+            img.setAttribute(
+              'src',
+              opts.src_root + '/assets/images/adopters/' + logo
+            );
+            img.setAttribute('class', 'adopters-img');
+
+            a.appendChild(img);
+            li.appendChild(a);
+            ul.appendChild(li);
+          }
+        }
+      }
+      for (var i = 0; i < el.length; i++) {
+        if (opts['ul_classes'] !== '') {
+          ul.setAttribute('class', opts['ul_classes']);
+        }
+        el[i].appendChild(ul);
+      }
+    }
+
+    // Function to scroll when there is anchor in url
+    scrollToAnchor() {
+      if (location.hash) {
+        var projectId = location.hash.replace('#', '');
+        var element = document.getElementById(`${projectId}`);
+        element.scrollIntoView();
+      }
     }
   }
 
-  // Function to scroll when there is anchor in url
-  scrollToAnchor() {
-    if (location.hash) {
-      var projectId = location.hash.replace('#', '');
-      var element = document.getElementById(`${projectId}`);
-      element.scrollIntoView();
-    }
-  }
-}
+  window.eclipseFdnAdopters = new EclipseFdnAdoptersClass();
+})(window, document);
 
-var eclipseFdnAdopters = new EclipseFdnAdopters();
-window.eclipseFdnAdopters = eclipseFdnAdopters;
-export default eclipseFdnAdopters;
+export default EclipseFdnAdopters;
--- js/privacy/eclipsefdn.videos.js
+++ js/privacy/eclipsefdn.videos.js
@@ -10,131 +10,138 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-const eclipseFdnVideos = (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['ef'], factory);
-  } else {
-    // Browser globals
-    root.eclipseFdnVideos = factory(root.ef);
-  }
-})(typeof self !== 'undefined' ? self : this, function (ef) {
-  'use strict';
+const eclipseFdnVideos = (function (window, document) {
+  (function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define(['ef'], factory);
+    } else {
+      // Browser globals
+      root.eclipseFdnVideos = factory(root.ef);
+    }
+  })(typeof self !== 'undefined' ? self : this, function (ef) {
+    'use strict';
 
-  // Define object
-  var eclipseFdnVideos = {};
+    // Define object
+    var eclipseFdnVideos = {};
 
-  /**
-   * Replace some videos
-   * @public
-   * @param {Object} options Videos attributes
-   */
-  eclipseFdnVideos.replace = function (options) {
-    // Default settings
-    const default_options = {
-      selector: '.eclipsefdn-video',
-      resolution: '16by9',
-      cookie: {
-        name: 'eclipse_cookieconsent_status',
-        value: 'allow',
-      },
+    /**
+     * Replace some videos
+     * @public
+     * @param {Object} options Videos attributes
+     */
+    eclipseFdnVideos.replace = function (options) {
+      // Default settings
+      const default_options = {
+        selector: '.eclipsefdn-video',
+        resolution: '16by9',
+        cookie: {
+          name: 'eclipse_cookieconsent_status',
+          value: 'allow',
+        },
+      };
+
+      // Go through the parameters of Options if its defined and is an object
+      if (typeof options !== 'undefined' && typeof options === 'object') {
+        for (const optionName in default_options) {
+          if (
+            optionName === 'cookie' &&
+            typeof options['cookie'] !== 'object'
+          ) {
+            continue;
+          }
+          if (
+            typeof options[optionName] === 'undefined' ||
+            typeof options[optionName] !== 'string'
+          ) {
+            continue;
+          }
+          if (
+            optionName === 'resolution' &&
+            (options['resolution'] !== '16by9' ||
+              options['resolution'] !== '4by3')
+          ) {
+            continue;
+          }
+          default_options[optionName] = options[optionName];
+        }
+      }
+
+      // Select all the videos containing the selector Class
+      this.el = document.querySelectorAll(default_options['selector']);
+
+      var cookie_value = '';
+      if (typeof default_options['cookie']['name'] !== 'undefined') {
+        // Get the cookie consent value
+        var value = '; ' + document.cookie;
+        var parts = value.split('; ' + default_options['cookie']['name'] + '=');
+        if (parts.length >= 2) {
+          cookie_value = parts.pop().split(';').shift();
+        }
+      }
+
+      for (var i = 0; i < this.el.length; i++) {
+        // Give the proper height to the element
+        var width = this.el[i].offsetWidth;
+        var resolution_sizes = default_options['resolution'].split('by');
+        var percentage = (resolution_sizes[1] / resolution_sizes[0]) * 100;
+
+        // Set the appropriate height for the video
+        this.el[i].setAttribute(
+          'style',
+          'height:' + (width * percentage) / 100 + 'px;'
+        );
+
+        // Making sure the url is formatted as we expect it to be
+        var link = this.el[i]
+          .getAttribute('href')
+          .replace(
+            /http(s|):\/\/(www|)(\.|)(youtube\.com\/watch\?v=|youtu\.be\/)/i,
+            '//www.youtube.com/watch?v='
+          );
+
+        if (cookie_value === default_options['cookie']['value']) {
+          // Create a div to wrap the viewo
+          var video_container = document.createElement('div');
+          video_container.className =
+            'eclipsefdn-video embed-responsive embed-responsive-' +
+            default_options['resolution'];
+          video_container.innerHTML =
+            '<iframe src="' +
+            link.replace(/watch\?v\=/i, 'embed/') +
+            '" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen" ></iframe>';
+
+          // Replace the original element with the embeded video
+          this.el[i].parentNode.replaceChild(video_container, this.el[i]);
+        } else {
+          this.el[i].setAttribute(
+            'class',
+            'eclipsefdn-video eclipsefdn-video-with-js'
+          );
+          if (this.el[i].getElementsByTagName('img').length != 1) {
+            var video_id = '';
+            if (link.includes('//www.youtube.com/watch?v=')) {
+              video_id = link.replace('//www.youtube.com/watch?v=', '');
+            }
+            if (link.includes('//www.youtube.com/embed/')) {
+              video_id = link.replace('//www.youtube.com/embed/', '');
+            }
+            if (video_id !== '') {
+              this.el[i].innerHTML =
+                '<img class="img-responsive" src="//img.youtube.com/vi/' +
+                video_id +
+                '/maxresdefault.jpg">';
+            }
+          }
+        }
+      }
     };
 
-    // Go through the parameters of Options if its defined and is an object
-    if (typeof options !== 'undefined' && typeof options === 'object') {
-      for (const optionName in default_options) {
-        if (optionName === 'cookie' && typeof options['cookie'] !== 'object') {
-          continue;
-        }
-        if (
-          typeof options[optionName] === 'undefined' ||
-          typeof options[optionName] !== 'string'
-        ) {
-          continue;
-        }
-        if (
-          optionName === 'resolution' &&
-          (options['resolution'] !== '16by9' ||
-            options['resolution'] !== '4by3')
-        ) {
-          continue;
-        }
-        default_options[optionName] = options[optionName];
-      }
-    }
+    return eclipseFdnVideos;
+  });
+  window.addEventListener('load', function () {
+    window.eclipseFdnVideos.replace();
+  });
+})(window, document);
 
-    // Select all the videos containing the selector Class
-    this.el = document.querySelectorAll(default_options['selector']);
-
-    var cookie_value = '';
-    if (typeof default_options['cookie']['name'] !== 'undefined') {
-      // Get the cookie consent value
-      var value = '; ' + document.cookie;
-      var parts = value.split('; ' + default_options['cookie']['name'] + '=');
-      if (parts.length >= 2) {
-        cookie_value = parts.pop().split(';').shift();
-      }
-    }
-
-    for (var i = 0; i < this.el.length; i++) {
-      // Give the proper height to the element
-      var width = this.el[i].offsetWidth;
-      var resolution_sizes = default_options['resolution'].split('by');
-      var percentage = (resolution_sizes[1] / resolution_sizes[0]) * 100;
-
-      // Set the appropriate height for the video
-      this.el[i].setAttribute(
-        'style',
-        'height:' + (width * percentage) / 100 + 'px;'
-      );
-
-      // Making sure the url is formatted as we expect it to be
-      var link = this.el[i]
-        .getAttribute('href')
-        .replace(
-          /http(s|):\/\/(www|)(\.|)(youtube\.com\/watch\?v=|youtu\.be\/)/i,
-          '//www.youtube.com/watch?v='
-        );
-
-      if (cookie_value === default_options['cookie']['value']) {
-        // Create a div to wrap the viewo
-        var video_container = document.createElement('div');
-        video_container.className =
-          'eclipsefdn-video embed-responsive embed-responsive-' +
-          default_options['resolution'];
-        video_container.innerHTML =
-          '<iframe src="' +
-          link.replace(/watch\?v\=/i, 'embed/') +
-          '" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen" ></iframe>';
-
-        // Replace the original element with the embeded video
-        this.el[i].parentNode.replaceChild(video_container, this.el[i]);
-      } else {
-        this.el[i].setAttribute(
-          'class',
-          'eclipsefdn-video eclipsefdn-video-with-js'
-        );
-        if (this.el[i].getElementsByTagName('img').length != 1) {
-          var video_id = '';
-          if (link.includes('//www.youtube.com/watch?v=')) {
-            video_id = link.replace('//www.youtube.com/watch?v=', '');
-          }
-          if (link.includes('//www.youtube.com/embed/')) {
-            video_id = link.replace('//www.youtube.com/embed/', '');
-          }
-          if (video_id !== '') {
-            this.el[i].innerHTML =
-              '<img class="img-responsive" src="//img.youtube.com/vi/' +
-              video_id +
-              '/maxresdefault.jpg">';
-          }
-        }
-      }
-    }
-  };
-
-  return eclipseFdnVideos;
-});
-
-window.eclipseFdnVideos.replace();
 export default eclipseFdnVideos;
